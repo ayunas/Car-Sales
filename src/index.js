059d22124223af27159d0cf300ec7eb8262c3767
Amir Yunas <ayunas@gmail.com>
2019-08-14 18:08:31 -0500

    set up redux store and connected App to the store, and mapped required state to the props

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import {createStore} from 'redux';
+import {reducer} from './components/reducer';
+import {Provider} from 'react-redux';
+
+const redux = createStore(reducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={redux}><App /></Provider>, rootElement);
